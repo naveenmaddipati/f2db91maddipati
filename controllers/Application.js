f5bb70d8ee50c10c1dbdd9cb7186a4fcf90432be
naveenmaddipati <111701781+naveenmaddipati@users.noreply.github.com>
2022-11-11 02:30:16 -0600

    added a file

--- controllers/Application.js
+++ controllers/Application.js
@@ -10,10 +10,18 @@ exports.Application_list = async function (req, res) {
         res.send(`{"error": ${err}}`);
     }
 };
-// for a specific Application.
-exports.Application_detail = function (req, res) {
-    res.send('NOT IMPLEMENTED: Application detail: ' + req.params.id);
-};
+// for a specific Application. 
+exports.Application_detail = async function(req, res) { 
+    console.log("detail"  + req.params.id) 
+    try { 
+        result = await Application.findById( req.params.id) 
+        res.send(result) 
+    } catch (error) { 
+        res.status(500) 
+        res.send(`{"error": document for id ${req.params.id} not found`); 
+    } 
+}; 
+
 // Handle Application create on POST.
 exports.Application_create_post = async function (req, res) {
     console.log(req.body)
@@ -55,3 +63,22 @@ exports.Application_view_all_Page = async function (req, res) {
         res.send(`{"error": ${err}}`);
     }
 };
+//Handle Application update form on PUT. 
+exports.Application_update_put = async function(req, res) { 
+    console.log(`update on id ${req.params.id} with body 
+${JSON.stringify(req.body)}`) 
+    try { 
+        let toUpdate = await Application.findById( req.params.id) 
+        // Do updates of properties 
+        if(req.body.App_Name) toUpdate.App_Name = req.body.App_Name; 
+        if(req.body.App_Company) toUpdate.App_Company = req.body.App_Company; 
+        if(req.body.App_Size) toUpdate.App_Size = req.body.App_Size; 
+        let result = await toUpdate.save(); 
+        console.log("Sucess " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": ${err}: Update for id ${req.params.id} 
+failed`); 
+    } 
+}; 
